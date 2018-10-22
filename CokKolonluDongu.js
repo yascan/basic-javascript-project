87f5f9a0b45df081e424edf4517388b58ede2340
Yaşar Can AKBAŞ <30414432+yascan@users.noreply.github.com>
2018-10-22 23:43:57 +0300

    Update CokKolonluDongu.js

--- CokKolonluDongu.js
+++ CokKolonluDongu.js
@@ -1,13 +1,12 @@
-for(var br=0;br<5;br++){
+for(var br=0;br<1;br++){
     for(var onbes=15;onbes<100;onbes+=15){
-        document.write(onbes);}
+        document.write(onbes + '</br>');}
     for(var bes=5;bes<=30;bes+=5){
-        document.writeln(bes);}
+        document.writeln(bes + '</br>');}
     for(var on=100;on>=50;on-=10){
-        document.writeln(on);}
+        document.writeln(on + '</br>');}
     for(var iki=2;iki<=64;iki*=2){
-        document.writeln(iki);
+        document.writeln(iki + '</br>');
     }
-    document.write("<br>");
-}       
-       
+    document.write("");
+}    
