3b4a94660f4ee1ed1d2c025a314e17281205635a
Yaşar Can AKBAŞ <30414432+yascan@users.noreply.github.com>
2018-10-22 23:41:05 +0300

    Create CokKolonluDongu.js

---
+++ CokKolonluDongu.js
@@ -0,0 +1,13 @@
+for(var br=0;br<5;br++){
+    for(var onbes=15;onbes<100;onbes+=15){
+        document.write(onbes);}
+    for(var bes=5;bes<=30;bes+=5){
+        document.writeln(bes);}
+    for(var on=100;on>=50;on-=10){
+        document.writeln(on);}
+    for(var iki=2;iki<=64;iki*=2){
+        document.writeln(iki);
+    }
+    document.write("<br>");
+}       
+       
